5b138e2727546c95459aeb3d6163989f3e09ef12
Abdullah Jamiu <95505963+roji-tech@users.noreply.github.com>
2022-08-17 01:53:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Services from "./components/Services";
+// import Services from "./components/Services";
 import Projects from "./components/Projects";
-import Testimonies from "./components/Testimonies";
+// import Testimonies from "./components/Testimonies";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
